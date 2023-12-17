5e2e62ac2a9d2255d9f307f58b4f24b308227e78
erwanrozet <erwan.rozet@nexelec.fr>
2023-12-17 12:32:15 +0100

    remove osme things

--- testfrontend/src/pages/test/mabite.tsx
+++
@@ -1,9 +0,0 @@
-// import { useTranslation } from 'next-i18next'
-// import { useMutation, useQuery, useQueryClient } from 'react-query'
-// import { useState } from 'react'
-
-const MyComponent: React.FC = () => {
-  return <div>hello ma bite</div>
-}
-
-export default MyComponent
